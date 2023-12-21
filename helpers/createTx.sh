3a9fdb389e6f1200c8f2c106c214e70a74241615
0xtekgrinder <0xtekgrinder@protonmail.com>
2023-12-21 08:52:13 -0500

    feat: add arguments to run createTx script

--- helpers/createTx.sh
+++ helpers/createTx.sh
@@ -78,42 +78,73 @@ function chain_to_chainId {
   esac
 }
 
+function usage {
+  echo "bash createTx.sh <script> <chain>"
+  echo ""
+  echo -e "script: path to the script to run"
+  echo -e "chain: chain(s) to run the script on (separate with commas)"
+  echo -e "\t1: Ethereum Mainnet"
+  echo -e "\t2: Arbitrum"
+  echo -e "\t3: Polygon"
+  echo -e "\t4: Gnosis"
+  echo -e "\t5: Avalanche"
+  echo -e "\t6: Base"
+  echo -e "\t7: Binance Smart Chain"
+  echo -e "\t8: Celo"
+  echo -e "\t9: Polygon ZkEvm"
+  echo -e "\t10: Optimism"
+  echo ""
+}
+
 function main {
+    command=false
+    if [[ $# -ne 2 && $# -ne 0 ]]; then
+        usage
+        exit 1
+    fi
+    if [ $# -eq 2 ]; then
+        script=$1
+        chains=$2
+        command=true
+    fi
+
     if [ ! -f .env ]; then
         echo ".env not found!"
         exit 1
     fi
     source .env
 
-    echo ""
-    echo "What script would you like to run ?"
+    if [ $command != true ]; then
+        echo ""
+        echo "What script would you like to run ?"
 
-    read script
+        read script
 
-    if [ -z "$script" ]; then
-        echo "No script provided"
-        exit 1
-    fi
+        if [ -z "$script" ]; then
+            echo "No script provided"
+            exit 1
+        fi
 
-    echo ""
+        echo ""
 
-    echo "Which chain(s) would you like to run the script on ? (separate with commas)"
-    echo "- 1: Ethereum Mainnet"
-    echo "- 2: Arbitrum"
-    echo "- 3: Polygon"
-    echo "- 4: Gnosis"
-    echo "- 5: Avalanche"
-    echo "- 6: Base"
-    echo "- 7: Binance Smart Chain"
-    echo "- 8: Celo"
-    echo "- 9: Polygon ZkEvm"
-    echo "- 10: Optimism"
+        echo "Which chain(s) would you like to run the script on ? (separate with commas)"
+        echo "- 1: Ethereum Mainnet"
+        echo "- 2: Arbitrum"
+        echo "- 3: Polygon"
+        echo "- 4: Gnosis"
+        echo "- 5: Avalanche"
+        echo "- 6: Base"
+        echo "- 7: Binance Smart Chain"
+        echo "- 8: Celo"
+        echo "- 9: Polygon ZkEvm"
+        echo "- 10: Optimism"
 
-    read chains
+        read chains
 
-    if [ -z "$chains" ]; then
-        echo "No chain provided"
-        exit 1
+        if [ -z "$chains" ]; then
+            echo "No chain provided"
+            exit 1
+        fi
     fi
 
     for chain in $(echo $chains | sed "s/,/ /g")
@@ -121,6 +152,13 @@ function main {
         echo ""
         echo "Running on chain $chain"
         uri=$(chain_to_uri $chain)
+
+        if [ -z "$uri" ]; then
+            echo ""
+            echo "Invalid chain"
+            continue
+        fi
+
         export CHAIN_ID=$(chain_to_chainId $chain)
         forge script $script --fork-url $uri
 
@@ -147,4 +185,4 @@ function main {
     done
 }
 
-main
+main $@
