f9e631a5fb35d564f507cd13f495f1ebc1291e24
0xtekgrinder <0xtekgrinder@protonmail.com>
2024-05-14 00:27:53 -0400

    tests: test if bridge works

--- test/angleTokens/ConnectAgTokenSideChainMultiBridges.t.sol
+++ test/angleTokens/ConnectAgTokenSideChainMultiBridges.t.sol
@@ -27,5 +27,55 @@ contract ConnectAgTokenSideChainMultiBridgesTest is BaseTest {
             (bool success, ) = gnosisSafe.call(abi.encode(address(to), payload, operation, 1e6));
             if (!success) revert();
         }
+
+        string memory stableName = vm.envString("STABLE_NAME");
+        uint256 chainId = vm.envUint("CHAIN_ID");
+        string memory json = vm.readFile(JSON_ADDRESSES_PATH);
+        address token = vm.parseJsonAddress(json, ".agToken");
+        address lzToken = vm.parseJsonAddress(json, ".lzAgToken");
+
+        (uint256[] memory chainIds, address[] memory contracts) = _getConnectedChains(stableName);
+        for (uint256 i = 0; i < contracts.length; i++) {
+            if (chainIds[i] == chainId || chainIds[i] == CHAIN_ETHEREUM) {
+                continue;
+            }
+
+            vm.selectFork(forkIdentifier[chainIds[i]]);
+            uint256 amount = 1e18;
+            address receiver = vm.addr(1);
+            bytes memory payload = abi.encode(abi.encodePacked(receiver), amount);
+
+            hoax(address(_lzEndPoint(chainIds[i])));
+            NonblockingLzApp(contracts[i]).lzReceive(
+                _getLZChainId(chainId),
+                abi.encodePacked(lzToken, contracts[i]),
+                0,
+                payload
+            );
+
+            address targetToken = _chainToContract(chainIds[i], ContractType.Angle);
+            assertEq(IERC20(targetToken).balanceOf(receiver), amount);
+        }
+
+        vm.selectFork(forkIdentifier[chainId]);
+        for (uint256 i = 0; i < contracts.length; i++) {
+            if (chainIds[i] == chainId) {
+                continue;
+            }
+
+            uint256 amount = 1e18;
+            address receiver = vm.addr(1);
+            bytes memory payload = abi.encode(abi.encodePacked(receiver), amount);
+
+            hoax(address(_lzEndPoint(chainId)));
+            NonblockingLzApp(lzToken).lzReceive(
+                _getLZChainId(chainIds[i]),
+                abi.encodePacked(contracts[i], lzToken),
+                0,
+                payload
+            );
+
+            assertEq(IERC20(token).balanceOf(receiver), amount);
+        }
     }
 }
--- test/angleTokens/ConnectAngleSideChainMultiBridges.t.sol
+++ test/angleTokens/ConnectAngleSideChainMultiBridges.t.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.8.19;
 
 import { MockSafe } from "../mock/MockSafe.sol";
 import { BaseTest } from "../BaseTest.t.sol";
+import { console } from "forge-std/console.sol";
+import { NonblockingLzApp } from "angle-tokens/agToken/layerZero/utils/NonblockingLzApp.sol";
+import { IERC20 } from "oz/token/ERC20/IERC20.sol";
 import "../../scripts/foundry/Constants.s.sol";
 
 contract ConnectAngleSideChainMultiBridgesTest is BaseTest {
@@ -26,6 +29,55 @@ contract ConnectAngleSideChainMultiBridgesTest is BaseTest {
             vm.prank(gnosisSafe);
             (bool success, ) = gnosisSafe.call(abi.encode(address(to), payload, operation, 1e6));
             if (!success) revert();
+        }
+
+        uint256 chainId = vm.envUint("CHAIN_ID");
+        string memory json = vm.readFile(JSON_ADDRESSES_PATH);
+        address lzToken = vm.parseJsonAddress(json, ".lzAngle");
+        address angle = vm.parseJsonAddress(json, ".angle");
+
+        (uint256[] memory chainIds, address[] memory contracts) = _getConnectedChains("ANGLE");
+        for (uint256 i = 0; i < contracts.length; i++) {
+            if (chainIds[i] == chainId || chainIds[i] == CHAIN_ETHEREUM) {
+                continue;
+            }
+
+            vm.selectFork(forkIdentifier[chainIds[i]]);
+            uint256 amount = 1e18;
+            address receiver = vm.addr(1);
+            bytes memory payload = abi.encode(abi.encodePacked(receiver), amount);
+
+            hoax(address(_lzEndPoint(chainIds[i])));
+            NonblockingLzApp(contracts[i]).lzReceive(
+                _getLZChainId(chainId),
+                abi.encodePacked(lzToken, contracts[i]),
+                0,
+                payload
+            );
+
+            address token = _chainToContract(chainIds[i], ContractType.Angle);
+            assertEq(IERC20(token).balanceOf(receiver), amount);
+        }
+
+        vm.selectFork(forkIdentifier[chainId]);
+        for (uint256 i = 0; i < contracts.length; i++) {
+            if (chainIds[i] == chainId) {
+                continue;
+            }
+
+            uint256 amount = 1e18;
+            address receiver = vm.addr(1);
+            bytes memory payload = abi.encode(abi.encodePacked(receiver), amount);
+
+            hoax(address(_lzEndPoint(chainId)));
+            NonblockingLzApp(lzToken).lzReceive(
+                _getLZChainId(chainIds[i]),
+                abi.encodePacked(contracts[i], lzToken),
+                0,
+                payload
+            );
+
+            assertEq(IERC20(angle).balanceOf(receiver), amount);
         }
     }
 }
