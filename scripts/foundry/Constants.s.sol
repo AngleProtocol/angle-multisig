ea32d12a962650de6e2aef13b7fc7a0891eb01a3
0xtekgrinder <0xtekgrinder@protonmail.com>
2024-06-21 15:56:07 +0200

    feat: blast deployment

--- scripts/foundry/Constants.s.sol
+++ scripts/foundry/Constants.s.sol
@@ -46,6 +46,11 @@ struct SafeTransaction {
     address safe;
 }
 
+struct MultiSendTransactions {
+    SafeTransaction transaction;
+    SafeTransaction[] internalTransactions;
+}
+
 /*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
                                                     CONTRACTS                                                    
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
@@ -63,6 +68,8 @@ MultiSend constant multiSendPolygonZkEVM = MultiSend(0x40A2aCCbd92BCA938b02010E1
 MultiSend constant multiSendLinea = MultiSend(0x40A2aCCbd92BCA938b02010E17A5b8929b49130D);
 MultiSend constant multiSendMantle = MultiSend(0xA1dabEF33b3B82c7814B6D82A79e50F4AC44102B);
 MultiSend constant multiSendMode = MultiSend(0x40A2aCCbd92BCA938b02010E17A5b8929b49130D);
+MultiSend constant multiSendBlast = MultiSend(0x40A2aCCbd92BCA938b02010E17A5b8929b49130D);
+
 // MultiSend constant multiSendStarknet = MultiSend();
 // MultiSend constant multiSendNear = MultiSend();
 // MultiSend constant multiSendSolana = MultiSend();
